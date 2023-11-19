bef91da815fec4556603cfad94b7ec50a586fadc
danito <info@danito.net>
2023-11-19 16:35:49 +0700

    Update battery-level.py

--- ups/waveshare-ups-module-3s/battery-level.py
+++ ups/waveshare-ups-module-3s/battery-level.py
@@ -18,21 +18,12 @@ ina1 = INA219(i2c_addr=0x41)
 ina1.begin()
 ina1.linear_calibrate(1000)
 
-bus_voltage = 0
-shunt_voltage = 0
-current = 0
-power = 0
-load_voltage = 0
-
 gui = GUI()
 gui.clear()
 
-battery_level = 0
-prev_battery_level = 0
-
 ## footer status bar
 
-# footer horizontal line (separator)
+# footer horizontal line
 gui.draw_line(x0=0, y0=288, x1=240, y1=288, width=1, color=(0, 0, 0))
 
 # footer vertical line (separator)
@@ -41,9 +32,10 @@ gui.draw_line(x0=96, y0=288, x1=96, y1=320, width=1, color=(0, 0, 0))
 gui.draw_line(x0=144, y0=288, x1=144, y1=320, width=1, color=(0, 0, 0))
 gui.draw_line(x0=192, y0=288, x1=192, y1=320, width=1, color=(0, 0, 0))
 
-stat = " "
+bus_voltage = 0
+battery_level = 0
 
-img_battery_level = gui.draw_image(x = 4, y = 295, w = 40, h = 20, image='/home/sAIns-digital/images/battery-level-0.png')
+img_battery_level = gui.draw_image(x = 4, y = 295, w = 40, h = 20, image = '/home/sAIns-digital/images/battery-level-0.png')
 val_battery_level = gui.draw_text(x=54, y=295, text = str(battery_level) + "%", color="black", font_size=10)
 
 while True:
@@ -56,23 +48,19 @@ while True:
       if(battery_level > 100):battery_level = 100
       if(battery_level < 0):battery_level = 0
 
-      if (prev_battery_level != battery_level):
+      if 0 <= battery_level < 21:
+         img_battery_level.config(image = '/home/sAIns-digital/images/battery-level-0.png')
+      elif 21 <= battery_level < 41:
+         img_battery_level.config(image = '/home/sAIns-digital/images/battery-level-20.png')
+      elif 41 <= battery_level < 61:
+         img_battery_level.config(image = '/home/sAIns-digital/images/battery-level-40.png')
+      elif 61 <= battery_level < 81:
+         img_battery_level.config(image = '/home/sAIns-digital/images/battery-level-60.png')
+      elif 61 <= battery_level < 81:
+         img_battery_level.config(image = '/home/sAIns-digital/images/battery-level-80.png')
+      else:
+         img_battery_level.config(image = '/home/sAIns-digital/images/battery-level-100.png')
 
-         if (battery_level  <= 100) & (battery_level  > 80):
-            img_battery_level.config(image='/home/sAIns-digital/images/battery-level-100.png')
-         elif (battery_level  <= 80) & (battery_level  > 60):
-            img_battery_level.config(image='/home/sAIns-digital/images/battery-level-80.png')
-         elif (battery_level  <= 60) & (battery_level  > 40):
-            img_battery_level.config(image='/home/sAIns-digital/images/battery-level-60.png')
-         elif (battery_level  <= 40) & (battery_level  > 20):
-            img_battery_level.config(image='/home/sAIns-digital/images/battery-level-40.png')
-         elif (battery_level  <= 20) & (battery_level  > 0):
-            img_battery_level.config(image='/home/sAIns-digital/images/battery-level-20.png')
-         else:
-            img_battery_level.config(image='/home/sAIns-digital/images/battery-level-0.png')
-
-         val_battery_level.config(text = str(battery_level) + "%")
-
-      prev_battery_level = battery_level
+      val_battery_level.config(text = str(battery_level) + "%")
 
       time.sleep(1)

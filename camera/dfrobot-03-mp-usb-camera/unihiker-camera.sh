6bccfe8faf3198f71a9320dd733237870698222d
danito <info@danito.net>
2023-11-19 07:25:19 +0700

    Update unihiker-camera.sh

--- camera/dfrobot-03-mp-usb-camera/unihiker-camera.sh
+++ camera/dfrobot-03-mp-usb-camera/unihiker-camera.sh
@@ -32,9 +32,11 @@ while :; do
       if [ ! -d $year/$month ]; then mkdir -p $year/$month; fi
       if [ ! -d $year/$month/$year$month$date/$hour ]; then mkdir -p $year/$month/$year$month$date/$hour; fi
 
+      cd $sdcard/$year/$month/$year$month$date/$hour
+
       # take a photo using fswebcam tools: PNG without compression, 1280 x 960 pixels, 270 degrees rotated
       # with chronological naming rule (example: /media/mmcblk0p1/2023/11/20231119/07/20231119-070432.png)
-      /usr/bin/fswebcam --no-banner --resolution 1280x960 --png 0 --rotate 270 $year/$month/$year$month$date/$hour/$year$month$date-$hour$minute$second.png
+      /usr/bin/fswebcam --no-banner --resolution 1280x960 --png 0 --rotate 270 $year$month$date-$hour$minute$second.png
 
       # just wait for a delay
       sleep $delay
